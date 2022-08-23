799816e7f89f29e1c28376f819ad7282887035d9
Howard Zhang <howard.zhang89@gmail.com>
2022-08-23 12:57:05 -0500

    Added basic Tkinter GUI.
    Basic GUI window with title, background color, size

---
+++ mybooks.py
@@ -0,0 +1,13 @@
+from tkinter import Tk, Button,Label,Scrollbar,Listbox,StringVar,Entry,W,E,N,S,END
+from tkinter import ttk
+from tkinter import messagebox
+
+root = Tk()
+
+root.title("My Books Database Application")
+root.configure(background="light green")
+root.geometry("850x500")
+root.resizable(width=False,height=False)
+
+#this will run the application
+root.mainloop()

abbd8ab86acdc7ec509f3bc2785b86727162c80a
Howard Zhang <howard.zhang89@gmail.com>
2022-08-23 17:52:16 -0500

    Add Delete, View, Clear, Exit buttons

--- mybooks.py
+++ mybooks.py
@@ -12,22 +12,22 @@ root.resizable(width=False,height=False)
 title_label = ttk.Label(root,text="Title",background="light green",font=("TkDefaultFont", 16))
 title_label.grid(row=0, column=0, sticky=W)
 title_text = StringVar()
-title_entry = ttk.Entry(root,width=14,textvariable=title_text)
+title_entry = ttk.Entry(root,width=24,textvariable=title_text)
 title_entry.grid(row=0, column=1, sticky=W)
 
 author_label = ttk.Label(root, text="Author",background="light green", font=("TkDefaultFont", 16))
 author_label.grid(row=0, column=2, sticky=W)
 author_text = StringVar()
-author_entry = ttk.Entry(root,width=14,textvariable=author_text)
+author_entry = ttk.Entry(root,width=24,textvariable=author_text)
 author_entry.grid(row=0, column=3, sticky=W)
 
 isbn_label = ttk.Label(root, text="ISBN",background="light green", font=("TkDefaultFont", 14))
 isbn_label.grid(row=0, column=4, sticky=W)
 isbn_text = StringVar()
-isbn_entry = ttk.Entry(root,width=14,textvariable=isbn_text)
+isbn_entry = ttk.Entry(root,width=24,textvariable=isbn_text)
 isbn_entry.grid(row=0, column=5, sticky=W)
 
-add_btn = Button(root, text="Add Book",bg="blue",fg="blue", font="helvetica 10 bold", command="")
+add_btn = Button(root, text="Add Book",bg="blue",fg="white", font="helvetica 10 bold", command="")
 add_btn.grid(row=0, column=6, sticky=W)
 
 list_bx = Listbox(root, height=16, width=40, font="helvetica 13", bg="light blue")
@@ -38,6 +38,21 @@ scroll_bar.grid(row=1, column=8, rowspan=14, sticky=W)
 
 list_bx.configure(yscrollcommand=scroll_bar.set)
 scroll_bar.configure(command=list_bx.yview)
+
+modify_btn = Button(root, text="Modify Record", bg="purple", fg="white", font="helvetica 10 bold", command="")
+modify_btn.grid(row=15, column=4)
+
+delete_btn = Button(root, text="Delete Record", bg="red", fg="white", font="helvetica 10 bold", command="")
+delete_btn.grid(row=15, column=5)
+
+view_btn = Button(root, text="View All Records", bg="black", fg="white", font="helvetica 10 bold", command="")
+view_btn.grid(row=15, column=1)
+
+clear_btn = Button(root, text="Clear Screen", bg="maroon", fg="white", font="helvetica 10 bold", command="")
+clear_btn.grid(row=15, column=2)
+
+exit_btn = Button(root, text="Modify Record", bg="blue", fg="white", font="helvetica 10 bold", command="")
+exit_btn.grid(row=15, column=3)
 
 #this will run the application
 root.mainloop()

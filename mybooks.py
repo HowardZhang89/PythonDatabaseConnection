c513359460a7934b3e58fe42aee04cfda8ada83a
Howard Zhang <howard.zhang89@gmail.com>
2022-08-23 18:03:10 -0500

    Add some descriptive comments

--- mybooks.py
+++ mybooks.py
@@ -2,13 +2,14 @@ from tkinter import Tk, Button,Label,Scrollbar,Listbox,StringVar,Entry,W,E,N,S,E
 from tkinter import ttk
 from tkinter import messagebox
 
-root = Tk()
+root = Tk() # Creates application window
 
 root.title("My Books Database Application")
 root.configure(background="light green")
 root.geometry("850x500")
-root.resizable(width=False,height=False)
+root.resizable(width=False,height=False) # Prevent application window from resizing
 
+# Create Labels and entry widgets
 title_label = ttk.Label(root,text="Title",background="light green",font=("TkDefaultFont", 16))
 title_label.grid(row=0, column=0, sticky=W)
 title_text = StringVar()
@@ -27,18 +28,22 @@ isbn_text = StringVar()
 isbn_entry = ttk.Entry(root,width=24,textvariable=isbn_text)
 isbn_entry.grid(row=0, column=5, sticky=W)
 
+# Add a button to insert inputs into database
 add_btn = Button(root, text="Add Book",bg="blue",fg="white", font="helvetica 10 bold", command="")
 add_btn.grid(row=0, column=6, sticky=W)
 
+# Add a listbox to display data from database
 list_bx = Listbox(root, height=16, width=40, font="helvetica 13", bg="light blue")
 list_bx.grid(row=3, column=1, columnspan=14, sticky=W + E, pady=40, padx=15)
 
+# Add scrollbar to enable scrolling
 scroll_bar = Scrollbar(root)
 scroll_bar.grid(row=1, column=8, rowspan=14, sticky=W)
 
-list_bx.configure(yscrollcommand=scroll_bar.set)
+list_bx.configure(yscrollcommand=scroll_bar.set) # Enables vertical scrolling
 scroll_bar.configure(command=list_bx.yview)
 
+# Add more Button Widgets
 modify_btn = Button(root, text="Modify Record", bg="purple", fg="white", font="helvetica 10 bold", command="")
 modify_btn.grid(row=15, column=4)
 
@@ -54,5 +59,5 @@ clear_btn.grid(row=15, column=2)
 exit_btn = Button(root, text="Modify Record", bg="blue", fg="white", font="helvetica 10 bold", command="")
 exit_btn.grid(row=15, column=3)
 
-#this will run the application
+#this will run the application until exit
 root.mainloop()

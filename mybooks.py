98d29f59640e8f5b3b4373e512d3d4d3a3ca1c24
Howard Zhang <howard.zhang89@gmail.com>
2022-08-24 11:40:41 -0500

    Add methods for add, update, delete, viewing new books

--- mybooks.py
+++ mybooks.py
@@ -20,6 +20,80 @@ class Bookdb:
     def __del__(self):
         self.con.close()
 
+    def view(self):
+        self.cursor.execute("Select * From books")
+        rows = self.cursor.fetchall()
+        return rows
+
+    def insert(self, title, author, isbn):
+        sql=("INSERT INTO books(title, author, isbn)VALUES (?,?,?)")
+        values = [title, author, isbn]
+        self.cursor.execute(sql, values)
+        self.cursor.commit()
+        messagebox.showinfo(title="Book Database", message="New book added to database")
+
+    def update(self, id, title, author, isbn):
+        tsql = 'UPDATE books SET title = ?, author = ?, isbn = ? WHERE id = ?'
+        self.cursor.execute(tsql, [title, author, isbn, id])
+        self.con.commit()
+        messagebox.showinfo(title="Book Database", message="Book Updated")
+
+    def delete(self, id):
+        delquery = 'DELETE FROM books WHERE id = ?'
+        self.cursor.execute(delquery, [id])
+        self.con.commit()
+        messagebox.showinfo(title="Book Database", message="Book Deleted")
+
+db = Bookdb
+
+def get_selected_row(event):
+    global selected_tuple
+    index = list_bx.curselection()[0]
+    selected_tuple = list_bx.get(index)
+    title_entry.delete(0,'end')
+    title_entry.insert('end', selected_tuple[1])
+    author_entry.delete(0, 'end')
+    author_entry.insert('end', selected_tuple[2])
+    isbn_entry.delete(0, 'end')
+    isbn_entry.insert('end', selected_tuple[3])
+
+def view_records():
+    list_bx.delete(0, 'end')
+    for row in db.view():
+        list_bx.insert('end', row)
+
+def add_book():
+    db.insert(title_text.get(), author_text.get(), isbn_text.get())
+    list_bx.delete(0, 'end')
+    list_bx.insert('end', (title_text.get(), author_text.get(), isbn_text.get()))
+    title_entry.delete(0, "end") #Clears input after inserting
+    author_entry.delete(0, "end")
+    isbn_entry.delete(0, "end")
+    con.commit()
+
+def delete_records():
+    db.delete(selected_tuple[0])
+    con.commit()
+
+def clear_screen():
+    list_bx.delete(0, 'end')
+    title_entry.delete(0,'end')
+    author_entry.delete(0,'end')
+    isbn_entry.delete(0,'end')
+
+def update_records():
+    db.update(selected_tuple[0], title_text.get(),author_text.get(), isbn_text.get())
+    title_entry.delete(0,"end")
+    author_entry.delete(0,"end")
+    isbn_entry.delete(0,"end")
+    con.commit()
+
+def on_closing():
+    dd = db
+    if messagebox.askokcancel("Quite", "Do you want to quit?"):
+        root.destroy()
+        del dd
+
 root = Tk() # Creates application window
 
 root.title("My Books Database Application")

d925fd2f5738ad0f0be8cf89d5edb9bf14d83ad9
Howard Zhang <howard.zhang89@gmail.com>
2022-08-24 11:49:38 -0500

    Bind actions to buttons

--- mybooks.py
+++ mybooks.py
@@ -9,7 +9,6 @@ print(con)
 
 cursor = con.cursor()
 
-
 class Bookdb:
     def __init__(self):
         self.con = pyo.connect(**dbConfig)
@@ -44,7 +43,7 @@ class Bookdb:
         self.con.commit()
         messagebox.showinfo(title="Book Database", message="Book Deleted")
 
-db = Bookdb
+db = Bookdb()
 
 def get_selected_row(event):
     global selected_tuple
@@ -102,6 +101,7 @@ root.geometry("850x500")
 root.resizable(width=False,height=False) # Prevent application window from resizing
 
 # Create Labels and entry widgets
+
 title_label = ttk.Label(root,text="Title",background="light green",font=("TkDefaultFont", 16))
 title_label.grid(row=0, column=0, sticky=W)
 title_text = StringVar()
@@ -121,12 +121,14 @@ isbn_entry = ttk.Entry(root,width=24,textvariable=isbn_text)
 isbn_entry.grid(row=0, column=5, sticky=W)
 
 # Add a button to insert inputs into database
-add_btn = Button(root, text="Add Book",bg="blue",fg="white", font="helvetica 10 bold", command="")
+
+add_btn = Button(root, text="Add Book",bg="blue",fg="white", font="helvetica 10 bold", command="add_book")
 add_btn.grid(row=0, column=6, sticky=W)
 
 # Add a listbox to display data from database
 list_bx = Listbox(root, height=16, width=40, font="helvetica 13", bg="light blue")
 list_bx.grid(row=3, column=1, columnspan=14, sticky=W + E, pady=40, padx=15)
+#list_bx.bind('<<ListboxSelect>>', get_selected_row)
 
 # Add scrollbar to enable scrolling
 scroll_bar = Scrollbar(root)
@@ -136,19 +138,20 @@ list_bx.configure(yscrollcommand=scroll_bar.set) # Enables vertical scrolling
 scroll_bar.configure(command=list_bx.yview)
 
 # Add more Button Widgets
-modify_btn = Button(root, text="Modify Record", bg="purple", fg="white", font="helvetica 10 bold", command="")
+
+modify_btn = Button(root, text="Modify Record", bg="purple", fg="white", font="helvetica 10 bold", command=update_records)
 modify_btn.grid(row=15, column=4)
 
-delete_btn = Button(root, text="Delete Record", bg="red", fg="white", font="helvetica 10 bold", command="")
+delete_btn = Button(root, text="Delete Record", bg="red", fg="white", font="helvetica 10 bold", command=delete_records)
 delete_btn.grid(row=15, column=5)
 
-view_btn = Button(root, text="View All Records", bg="black", fg="white", font="helvetica 10 bold", command="")
+view_btn = Button(root, text="View All Records", bg="black", fg="white", font="helvetica 10 bold", command=view_records)
 view_btn.grid(row=15, column=1)
 
-clear_btn = Button(root, text="Clear Screen", bg="maroon", fg="white", font="helvetica 10 bold", command="")
+clear_btn = Button(root, text="Clear Screen", bg="maroon", fg="white", font="helvetica 10 bold", command=clear_screen)
 clear_btn.grid(row=15, column=2)
 
-exit_btn = Button(root, text="Modify Record", bg="blue", fg="white", font="helvetica 10 bold", command="")
+exit_btn = Button(root, text="Modify Record", bg="blue", fg="white", font="helvetica 10 bold", command=root.destroy)
 exit_btn.grid(row=15, column=3)
 
 #this will run the application until exit
